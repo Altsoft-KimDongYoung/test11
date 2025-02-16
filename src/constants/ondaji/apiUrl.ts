a8eb69bae9b0392d99a377e92057118f0ebcf136
Altsoft-KimDongYoung <kim.dy@altsoft.kr>
2025-02-17 01:58:26 +0900

    5

--- src/constants/ondaji/apiUrl.ts
+++ src/constants/ondaji/apiUrl.ts
@@ -226,5 +226,11 @@ export const API_URL = {
     TEST: 'test',
     TEST2: 'test2',
     TEST3: 'test3',
+    TEST4: 'test4',
+    TEST5: 'test5',
+    TEST6: 'test6',
+    TEST7: 'test7',
+    TEST8: 'test8',
+    TEST9: 'test9',
   },
 } as const;
