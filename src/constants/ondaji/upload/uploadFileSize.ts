49de47af0c7cad5ea7cbdad1d4561ed0d87e3ebc
Altsoft-KimDongYoung <kim.dy@altsoft.kr>
2025-02-16 12:58:17 +0900

    dd

--- src/constants/ondaji/upload/uploadFileSize.ts
+++
@@ -1,6 +0,0 @@
-/** 콘텐츠 도메인에서 파일 업로드 시 허용되는 용량 */
-export const CONTENT_UPLOAD_FILE_SIZE = {
-  REPRESENT_FILE: '20MB',
-  GROUPING_IMAGE: '20MB',
-  GROUPING_VIDEO: '320MB',
-} as const;
--- src/constants/ondaji/upload/uploadFileType.ts
+++
@@ -1,4 +0,0 @@
-/** 콘텐츠 도메인에서 파일 업로드 시 허용되는 확장자 */
-export const IMAGE_FILE_BASE_TYPES = ['image/png', 'image/jpeg', 'image/jpg'];
-export const IMAGE_FILE_WITH_GIF_TYPES = [...IMAGE_FILE_BASE_TYPES, 'image/gif'];
-export const VIDEO_FILE_TYPES = ['video/mp4', 'video/quicktime'];
