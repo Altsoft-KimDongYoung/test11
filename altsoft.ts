f633b12d33f320340a7f54856002024c4a861403
Altsoft-KimDongYoung <kim.dy@altsoft.kr>
2025-02-16 12:42:16 +0900

    Squashed 'src/constants/ondaji/' content from commit ef31c98
    git-subtree-dir: src/constants/ondaji
    git-subtree-split: ef31c9871d8e3c7182fb72ee3c9fce9f2e0de418

---
+++ altsoft.ts
@@ -0,0 +1,10 @@
+import { Coordinate, Latitude, Longitude } from '@/ondaji/types/common';
+
+/** TODO: 상용화시 실제 문의 전화번호, 이메일 기입이 필요합니다. */
+export const ALTSOFT = {
+  ADDRESS: '02-0000-0000',
+  EMAIL: 'ondaji@altsoft.com',
+  LONGITUDE: 127.0447374 as Longitude,
+  LATITUDE: 37.5120885 as Latitude,
+  COORDINATE: [37.5120885, 127.0447374] as Coordinate,
+};
